5172277fa6daab6806f4496866e025c74cfd8dab
DZamb12 <daryllmatthews.zambrano@lorma.edu>
2023-12-15 20:42:47 +0800

    Main Screen buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playGame = new System.Windows.Forms.Button();
+            this.ExitApp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playGame
+            // 
+            this.playGame.Location = new System.Drawing.Point(310, 230);
+            this.playGame.Name = "playGame";
+            this.playGame.Size = new System.Drawing.Size(176, 35);
+            this.playGame.TabIndex = 0;
+            this.playGame.Text = "Play Game";
+            this.playGame.UseVisualStyleBackColor = true;
+            // 
+            // ExitApp
+            // 
+            this.ExitApp.Location = new System.Drawing.Point(310, 270);
+            this.ExitApp.Name = "ExitApp";
+            this.ExitApp.Size = new System.Drawing.Size(176, 35);
+            this.ExitApp.TabIndex = 1;
+            this.ExitApp.Text = "Exit";
+            this.ExitApp.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 73);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Frieren Adventures";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExitApp);
+            this.Controls.Add(this.playGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frieren Adventure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button playGame;
+        private System.Windows.Forms.Button ExitApp;
+        private System.Windows.Forms.Label label1;
     }
 }
 
